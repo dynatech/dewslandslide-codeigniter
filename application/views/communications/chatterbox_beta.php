40bf97411e1a1ae31a2a3f38988f1ced6982471c
John Geliberte <geliberteofficials@gmail.com>
2018-09-27 10:31:53 +0800

    Fixed missing plgu in add contacts

--- application/views/communications/chatterbox_beta.php
+++ application/views/communications/chatterbox_beta.php
@@ -462,12 +462,13 @@
 				    <div id="org-accord" class="panel-collapse collapse">
 				      <div class="panel-body">
 				      	<div id="organization-selection-div">
-				      		<div id="orgs-cc-0" class="col-md-2 col-sm-2 col-xs-2"></div>
-			                <div id="orgs-cc-1" class="col-md-2 col-sm-2 col-xs-2"></div>
-			                <div id="orgs-cc-2" class="col-md-2 col-sm-2 col-xs-2"></div>
-			                <div id="orgs-cc-3" class="col-md-2 col-sm-2 col-xs-2"></div>
-			                <div id="orgs-cc-4" class="col-md-2 col-sm-2 col-xs-2"></div>
-			                <div id="orgs-cc-5" class="col-md-2 col-sm-2 col-xs-2"></div>
+				      		<div id="orgs-cc-0" class="col-md-3 col-sm-3 col-xs-3"></div>
+			                <div id="orgs-cc-1" class="col-md-3 col-sm-3 col-xs-3"></div>
+			                <div id="orgs-cc-2" class="col-md-3 col-sm-3 col-xs-3"></div>
+			                <div id="orgs-cc-3" class="col-md-3 col-sm-3 col-xs-3"></div>
+			                <div id="orgs-cc-4" class="col-md-3 col-sm-3 col-xs-3"></div>
+			                <div id="orgs-cc-5" class="col-md-3 col-sm-3 col-xs-3"></div>
+			                <div id="orgs-cc-6" class="col-md-3 col-sm-3 col-xs-3"></div>
 				      	</div>
 				      </div>
 				    </div>
