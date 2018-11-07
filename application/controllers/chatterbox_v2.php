c617b06eff9e3aa096c023e06d26dfb8d779f0ca
jhndvdgvr <david063095@gmail.com>
2018-11-07 16:36:16 +0800

    initial push on chatterbox over haul

--- application/controllers/chatterbox_v2.php
+++ application/controllers/chatterbox_v2.php
@@ -10,17 +10,17 @@ class Chatterbox_v2 extends CI_Controller {
 	}
 	public function index() {
 		$this->is_logged_in();
-		$page = 'Chatterbox';
+		$page = 'Chatterbox V2';
 		$data['first_name'] = $this->session->userdata('first_name');
 		$data['last_name'] = $this->session->userdata('last_name');
 		$data['user_id'] = $this->session->userdata("id");
 		
 		$data['title'] = $page;
-		$this->load->view('templates/header', $data);
-		$this->load->view('templates/nav');
+		$this->load->view('templates/beta/header', $data);
+        $this->load->view('templates/beta/nav');
 		$this->load->view('communications/handlebars-chatterbox_beta');
 		$this->load->view('communications/chatterbox_v2');
-		$this->load->view('templates/footer');
+		$this->load->view('templates/beta/footer');
 	}
 	public function is_logged_in() {
 		$is_logged_in = $this->session->userdata('is_logged_in');
