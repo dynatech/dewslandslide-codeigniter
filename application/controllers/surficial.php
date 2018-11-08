30e23366cdd44164346bafca0306e88357ee81e6
John Louie Nepomuceno <jlouienepomuceno@gmail.com>
2018-11-08 18:45:39 +0800

    Replaced links for login.

--- application/controllers/surficial.php
+++ application/controllers/surficial.php
@@ -182,7 +182,7 @@ class Surficial extends CI_Controller {
 		$is_logged_in = $this->session->userdata('is_logged_in');
 		
 		if(!isset($is_logged_in) || ($is_logged_in !== TRUE)) {
-			echo 'You don\'t have permission to access this page. <a href="../lin">Login</a>';
+			echo 'You don\'t have permission to access this page. <a href="../login">Login</a>';
 			die();
 		}
 		else {
