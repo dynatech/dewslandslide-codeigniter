4959741b0d0638aaa139bbcbc0514c510eee8877
John Louie Nepomuceno <jlouienepomuceno@gmail.com>
2018-11-09 16:41:26 +0800

    Custom error message for page not found errors.

---
+++ application/views/pages/error_custom_404.php
@@ -0,0 +1,19 @@
+<?php 
+	$is_logged_in = $this->session->userdata('is_logged_in');
+	$url = $is_logged_in ? "/login" : "/home";
+?>
+<div id="page-wrapper">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6 text-center">
+            	<!-- image from https://banner2.kisspng.com/20180212/gje/kisspng-civil-engineering-surveyor-with-helmet-surveyor-surveyor-5a819ef2cf2877.5685038115184442748485.jpg -->
+				<img src="images/error-404.jpg" style="width: 55.3%; height: auto;">
+            </div>
+            <div class="col-sm-6 text-left">
+				<span><h1><strong>Well this is embarassing.</strong></h1></span>
+				<span><h3>We have Code 404. <br>Our survery squad could not find what you are looking for.</h3></span>
+				<a href="<?php echo $url; ?>"><button class="btn btn-primary">Return to Safety</button></a>
+            </div>
+        </div>
+    </div>
+</div>
