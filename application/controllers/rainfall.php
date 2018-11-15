fdecbaeedd7c60e651aecf1b536395976e39103a
jhndvdgvr <david063095@gmail.com>
2018-11-15 17:54:29 +0800

    change anacoda path

--- application/controllers/rainfall.php
+++ application/controllers/rainfall.php
@@ -37,8 +37,8 @@ class Rainfall extends CI_Controller {
             $python_path = "C:/Users/Dynaslope/Anaconda2/python.exe";
             $file_path = "C:/xampp/updews-pycodes/web_plots/";
         } elseif (strpos($os, "UBUNTU") !== false || strpos($os, "Linux") !== false) {
-            $python_path = "/home/jdguevarra/anaconda2/bin/python";
-            // $python_path = "/home/swat/anaconda2/bin/python";
+            // $python_path = "/home/jdguevarra/anaconda2/bin/python";
+            $python_path = "/home/ubuntu/anaconda2/bin/python";
             $file_path = "/var/www/updews-pycodes/web_plots/";
         } else {
             throw new Exception("Unknown OS for execution... Script discontinued...");
